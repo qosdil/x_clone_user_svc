39496c9c305b828d9c17113fe7447dad374e52ea
Qosdil A <qosdil@yahoo.com>
2024-10-09 08:53:32 +0700

    Add a comment.

--- repository/repository.go
+++ repository/repository.go
@@ -6,6 +6,7 @@ import (
 	"github.com/qosdil/x_clone_user_svc/model"
 )
 
+// Repository follows gORM convention for the method namings
 type Repository interface {
 	Create(ctx context.Context, user model.User) (model.User, error)
 	Find(ctx context.Context) (users []model.SecureUser, err error)
